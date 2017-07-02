1ddc95f70e6bd17c8dffb1be272d4ad1780d9e6d
이택규(Tackgyu Lee)/App개발팀/SKP <tackgyu.lee@sk.com>
2017-07-02 15:37:54 +0900

    HeaderView.vue 기본 레이아웃 추가

--- src/view/component/HeaderView.vue
+++ src/view/component/HeaderView.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    header-view
+  <div class="header-view">
+
+    <a @click="onClickLeftMenuButton()">left</a>
+
+    <div>title</div>
+
+    <a @click="onClickRightMenuButton()">right</a>
+
   </div>
 </template>
 
 <script>
   export default {
-    
+    methods: {
+      onClickLeftMenuButton() {
+        
+      },
+      onClickRightMenuButton() {
+
+      },
+    },
   }
 </script>
 
 <style scoped lang="scss">
-  
+  .header-view {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid $color-lightgray;
+  }
 </style>
