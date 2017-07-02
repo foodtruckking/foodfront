a10961e4922b21d058fda4863d2f234b0ef66710
이택규(Tackgyu Lee)/App개발팀/SKP <tackgyu.lee@sk.com>
2017-07-02 17:49:53 +0900

    DetailView 목업 추가

--- src/view/component/DetailView.vue
+++ src/view/component/DetailView.vue
@@ -7,6 +7,23 @@
       @onClickLeftButton="onClickBackButton()"
       />
 
+    <div class="thumbnail"></div>
+
+    <div class="title">
+      title
+    </div>
+
+    <div class="description">
+      description
+    </div>
+
+    <div ref="map-wrapper" class="map-wrapper" />
+
+    <a class="map-button"
+      @click="onClickMapButton()">
+      <div>view on the map</div>
+    </a>
+
   </div>
 </template>
 
@@ -17,11 +34,29 @@
     components: {
       HeaderView,
     },
+    mounted() {
+      this.initDetailMap()
+    },
     methods: {
       ...Vuex.mapMutations([
+        'setShownList',
         'setShownDetail',
       ]),
       onClickBackButton() {
+        this.setShownDetail(false)
+      },
+      initDetailMap() {
+        const wrapper = this.$refs['map-wrapper']
+
+        new google.maps.Map(wrapper, {
+          center: {lat: 33.396, lng: 126.570},
+          zoom: 12,
+          disableDefaultUI: true,
+          gestureHandling: 'none',
+        })
+      },
+      onClickMapButton() {
+        this.setShownList(false)
         this.setShownDetail(false)
       },
     },
@@ -29,8 +64,42 @@
 </script>
 
 <style scoped lang="scss">
+  @import '~scss/defines';
+
+  $thumbnail-height: 280px;
+  $map-height: 200px;
+  $map-button-height: 40px;
+
   .detail-view {
     height: 100%;
     background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+  }
+
+  .thumbnail {
+    height: $thumbnail-height;
+    border: 1px solid red; /* testable */
+  }
+
+  .title {
+
+  }
+
+  .description {
+
+  }
+
+  .map-wrapper {
+    height: $map-height;
+    pointer-events: none;
+  }
+
+  .map-button {
+    height: $map-button-height;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
